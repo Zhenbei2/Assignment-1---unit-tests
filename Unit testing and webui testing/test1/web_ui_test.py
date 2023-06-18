dc9b208df532317f24d1d9d97d9e1a08d04736cb
Zhenbei2 <103932894+Zhenbei2@users.noreply.github.com>
2023-06-18 13:24:50 +0700

    Update web_ui_test.py

--- Unit testing and webui testing/test1/web_ui_test.py
+++ Unit testing and webui testing/test1/web_ui_test.py
@@ -1,34 +1,55 @@
+import unittest
+import coverage
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.by import By
 
-# 启动浏览器
-driver = webdriver.Chrome()
+class WebUITest(unittest.TestCase):
+    def setUp(self):
+        # 启动浏览器
+        self.driver = webdriver.Chrome()
 
-try:
-    # 导航到应用程序主页
-    driver.get("http://localhost:5000")
+    def tearDown(self):
+        # 关闭浏览器
+        self.driver.quit()
 
-    # 找到输入框并输入测试字符串
-    input_element = driver.find_element(By.NAME, "input_string")
-    input_element.send_keys("aabbcc")
+    def test_web_ui(self):
+        # 导航到应用程序主页
+        self.driver.get("http://localhost:5000")
 
-    # 提交表单
-    input_element.send_keys(Keys.ENTER)
+        # 找到输入框并输入测试字符串
+        input_element = self.driver.find_element(By.NAME, "input_string")
+        input_element.send_keys("aabbcc")
 
-    # 等待结果元素加载
-    result_element = WebDriverWait(driver, 10).until(
-        EC.presence_of_element_located((By.ID, "result"))
-    )
+        # 提交表单
+        input_element.send_keys(Keys.ENTER)
 
-    # 获取结果文本
-    result = result_element.text
+        # 等待结果元素加载
+        result_element = WebDriverWait(self.driver, 10).until(
+            EC.presence_of_element_located((By.ID, "result"))
+        )
 
-    # 断言结果是否符合预期
-    assert result == "expected result"
+        # 获取结果文本
+        result = result_element.text
 
-finally:
-    # 关闭浏览器
-    driver.quit()
+        # 断言结果是否符合预期
+        self.assertEqual(result, "expected result")
+
+if __name__ == '__main__':
+    # 创建Coverage对象
+    cov = coverage.Coverage()
+    # 启动覆盖率监测
+    cov.start()
+
+    # 运行测试
+    suite = unittest.TestLoader().loadTestsFromTestCase(WebUITest)
+    unittest.TextTestRunner(verbosity=2).run(suite)
+
+    # 停止覆盖率监测并生成报告
+    cov.stop()
+    cov.save()
+    cov.report()
+    cov.html_report(directory='htmlcov')
+

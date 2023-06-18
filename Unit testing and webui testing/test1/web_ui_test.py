8488a81a1194f2dcdf2f248131a7b8a1134f47c4
Zhenbei2 <103932894+Zhenbei2@users.noreply.github.com>
2023-06-18 14:45:40 +0700

    Update web_ui_test.py

--- Unit testing and webui testing/test1/web_ui_test.py
+++ Unit testing and webui testing/test1/web_ui_test.py
@@ -15,26 +15,61 @@ class WebUITest(unittest.TestCase):
         # 关闭浏览器
         self.driver.quit()
 
-    def test_web_ui(self):
-        # 导航到应用程序主页
-        self.driver.get("http://localhost:5000")
+    def wait_for_page_load(self):
+        WebDriverWait(self.driver, 1).until(EC.presence_of_element_located((By.TAG_NAME, "body")))
 
-        # 找到输入框并输入测试字符串
-        input_element = self.driver.find_element(By.NAME, "input_string")
+    def test_web_ui_1(self):
+        self.driver.get("http://localhost:5000")
+        
+        self.wait_for_page_load()
+
+        input_element = self.driver.find_element(By.CSS_SELECTOR, "input[name='input_string']")
         input_element.send_keys("aabbcc")
 
-        # 提交表单
-        input_element.send_keys(Keys.ENTER)
+        input_element.submit()
 
-        # 等待结果元素加载
         result_element = WebDriverWait(self.driver, 10).until(
             EC.presence_of_element_located((By.ID, "result"))
         )
 
-        # 获取结果文本
         result = result_element.text
 
-        # 断言结果是否符合预期
+        self.assertEqual(result, "expected result")
+
+    def test_web_ui_2(self):
+        self.driver.get("http://localhost:5000")
+        
+        self.wait_for_page_load()
+
+        input_element = self.driver.find_element(By.CSS_SELECTOR, "input[name='input_string']")
+        input_element.send_keys("xyz")
+
+        input_element.submit()
+
+        result_element = WebDriverWait(self.driver, 1).until(
+            EC.presence_of_element_located((By.ID, "result"))
+        )
+
+        result = result_element.text
+
+        self.assertEqual(result, "expected result")
+
+    def test_web_ui_3(self):
+        self.driver.get("http://localhost:5000")
+        
+        self.wait_for_page_load()
+
+        input_element = self.driver.find_element(By.CSS_SELECTOR, "input[name='input_string']")
+        input_element.send_keys("12345")
+
+        input_element.submit()
+
+        result_element = WebDriverWait(self.driver, 1).until(
+            EC.presence_of_element_located((By.ID, "result"))
+        )
+
+        result = result_element.text
+
         self.assertEqual(result, "expected result")
 
 if __name__ == '__main__':
@@ -45,11 +80,11 @@ if __name__ == '__main__':
 
     # 运行测试
     suite = unittest.TestLoader().loadTestsFromTestCase(WebUITest)
-    unittest.TextTestRunner(verbosity=2).run(suite)
+    runner = unittest.TextTestRunner(verbosity=2)
+    runner.run(suite)
 
     # 停止覆盖率监测并生成报告
     cov.stop()
     cov.save()
     cov.report()
     cov.html_report(directory='htmlcov')
-
